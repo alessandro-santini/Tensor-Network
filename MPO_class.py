d4728781d612c2617cfaefd9fc2bc5fbd94b42f5
Alessandro Santini <76155864+Alessandro-Santini@users.noreply.github.com>
2021-09-06 14:27:36 +0200

    New

--- MPO_class.py
+++ MPO_class.py
@@ -32,62 +32,6 @@ class MPO:
            Rtemp = contract.contract_right(MPS.M[i], self.W[i], MPS.M[i].conj(), Rtemp)
        return contract.contract_right(MPS.M[0], self.W[0], MPS.M[0].conj(), Rtemp)[0][0][0]
         
-def IsingMPO(L,h, J=1):
-    Ham = MPO(L,2)
-        
-    # Pauli Matrices
-    Sx = np.array([[0, 1], [1, 0]])
-    Sz = np.array([[1, 0], [0,-1]])
-    Id = np.array([[1, 0], [0, 1]])
-    
-    # Building the local bulk MPO
-    H = np.zeros([3,3,2,2])
-    H[0,0,:,:] = Id; H[2,2,:,:] = Id; H[2,0,:,:] = -h*Sz
-    H[1,0,:,:] = Sx; H[2,1,:,:]= -J*Sx
-
-    # Building the boundary MPOs
-    HL = np.zeros((1,3,2,2))
-    HL[0,:,:,:] = H[2,:,:,:]
-    HR = np.zeros((3,1,2,2))
-    HR[:,0,:,:] = H[:,0,:,:]
-
-    Ham.W[0] = HL
-    Ham.W[L-1] = HR
-    for i in range(1,L-1):
-        Ham.W[i] = H
-    return Ham
-    
-def XXZMPO(L,delta, h, J=1):
-    Ham = MPO(L,2)
-    # Pauli Matrices
-    Sp = np.array([[0,1],[0,0]])
-    Sm = np.array([[0,0],[1,0]])
-    Sz = 0.5*np.array([[1, 0], [0,-1]])
-    Id = np.array([[1, 0], [0, 1]])
-    
-    # Building the local bulk MPO
-    H = np.zeros([5,5,2,2])
-    H[0,0,:,:] =       Id
-    H[1,0,:,:] =       Sp
-    H[2,0,:,:] =       Sm
-    H[3,0,:,:] =       Sz
-    H[4,0,:,:] =    -h*Sz
-    H[4,1,:,:] =    .5*J*Sm
-    H[4,2,:,:] =    .5*J*Sp
-    H[4,3,:,:] = delta*Sz
-    H[4,4,:,:] =       Id
-    
-    # Building the boundary MPOs
-    HL = np.zeros((1,5,2,2))
-    HL[0,:,:,:] = H[4,:,:,:]
-    HR = np.zeros((5,1,2,2))
-    HR[:,0,:,:] = H[:,0,:,:]
-    
-    Ham.W[0] = HL
-    Ham.W[L-1] = HR
-    for i in range(1,L-1):
-        Ham.W[i] = H
-    return Ham
 
 def getMzMPO(L):
     MzMPO = MPO(L,2)
---
+++ SpinSystems_2d.py
@@ -0,0 +1,47 @@
+import MPO_class as MPO
+import numpy as np
+
+def IsingMPO_2D(Lx, Ly, h=0., J=1):
+    
+    L = Lx*Ly
+    Ham = MPO.MPO(L, 2)
+        
+    # Pauli Matrices
+    Sx = np.array([[0, 1], [1, 0]])
+    Sz = np.array([[1, 0], [0,-1]])
+    
+    for i in range(1, L-1):
+        H = np.zeros([2+Ly, 2+Ly, 2, 2])
+        H[0, 0, :, :] = np.eye(2)
+        H[1, 0, :, :] = Sz
+        for j in range(1, Ly):
+            H[1+j,j,:,:] = np.eye(2)
+        H[-1,-2,:,:]  = -J*Sz
+        if i%Ly != 0:
+            H[-1,1,:,:]   = -J*Sz
+        H[-1,0,:,:]  = -h*Sx
+        H[-1,-1,:,:] = np.eye(2)
+        Ham.W[i] = H
+    
+    HL = np.zeros([1,2+Ly,2,2])
+    HR = np.zeros([2+Ly,1,2,2])
+    
+    HL[0,0, :,:] = -h*Sx
+    HL[0,1, :,:] = -J*Sz
+    HL[0,-2,:,:] = -J*Sz
+    HL[0,-1,:,:] = np.eye(2)
+    
+    HR[0, 0,:,:] = np.eye(2)
+    HR[1, 0,:,:] = Sz
+    HR[-1,0,:,:] = -h*Sx
+     
+    Ham.W[0]  = HL
+    Ham.W[-1] = HR
+    return Ham
+
+Lx = 4; Ly=Lx
+H = IsingMPO_2D(Lx, Ly,1,0.5)
+for i in range(2+Ly):
+    for j in range(2+Ly):
+        print(i,j)
+        print(H.W[1][i,j,:,:])
