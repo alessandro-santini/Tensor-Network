011fe8bac58236408a0b3978a0b24c19d4cf0210
Alessandro Santini <76155864+Alessandro-Santini@users.noreply.github.com>
2021-10-14 17:27:10 +0200

    commit

--- SpinSystems_2d.py
+++ SpinSystems_2d.py
@@ -44,3 +44,159 @@ def IsingMPO_2D(Lx, Ly, h=0., J=1):
     Ham.W[0]  = HL
     Ham.W[-1] = HR
     return Ham
+
+def IsingMPO_2D_Spiral_MPS(L, h=0., J=1):
+    def spiral(rows, columns):
+        matrix = np.zeros((rows,columns))
+        row, column, value = 0, 0, rows*columns-1
+        
+        while row < rows and column < columns:
+            for i in range(column, columns):
+                matrix[row,i] = value
+                value -= 1
+            row += 1
+            for i in range(row, rows):
+                matrix[i,columns - 1] = value
+                value -= 1
+            columns -= 1
+            if row < rows:
+                for i in range(columns - 1, column - 1, -1):
+                    matrix[rows - 1,i] = value
+                    value -= 1
+                rows -= 1
+            if column < columns:
+                for i in range(rows - 1, row - 1, -1):
+                    matrix[i,column] = value
+                    value -= 1
+                column += 1
+        return matrix    
+    def compute_dim_MPO(L):
+        config = spiral(L,L)
+        dim = []
+        nearest = []
+        for x in range(L*L):
+            i,j = np.where(config == x)
+            i = i[0]; j = j[0]
+            nn = []
+            if i !=0: nn.append(config[i-1,j])
+            if j !=0: nn.append(config[i,j-1])
+            if i !=L-1: nn.append(config[i+1,j])
+            if j !=L-1: nn.append(config[i,j+1])
+            nn = np.array(nn)
+            nearest.append( np.array(nn[nn>config[i,j]]-config[i,j],int))
+            new_dim = int(np.max(nn-config[i,j]) + 2)
+            if x > 0:
+                if new_dim < dim[x-1][1]:
+                   dim.append((dim[x-1][1],dim[x-1][1]-1,2,2))
+                else:
+                    dim.append((dim[x-1][1],new_dim,2,2))    
+            else:
+                dim.append((1,new_dim,2,2))
+        dim[-1] = (dim[-2][1],1,2,2)
+        return dim,nearest
+    
+    Ham = MPO.MPO(L*L, 2)
+    Ham.config = np.array(spiral(L,L),int)
+    # Pauli Matrices
+    Sx = np.array([[0, 1], [1, 0]])
+    Sz = np.array([[1, 0], [0,-1]])
+
+    dimW,nn = compute_dim_MPO(L)
+
+    for i,dim in enumerate(dimW):
+        Ham.W[i] = np.zeros(dim)
+    
+    # Left Bond
+    Ham.W[0][0,0,:,:] = - h*Sx
+    for k in nn[0]:
+        Ham.W[0][0,k,:,:] = -J*Sz
+    Ham.W[0][0,-1,:,:] = np.eye(2)
+    # Right Bond
+    Ham.W[-1][0,0,:,:] = np.eye(2)
+    Ham.W[-1][1,0,:,:] = Sz
+    Ham.W[-1][2,0,:,:] = -h*Sx    
+    # Bulk Bonds
+    for i in range(1,L**2-1):
+        Ham.W[i][0,0,:,:]  = np.eye(2)
+        Ham.W[i][-1,0,:,:] = -h*Sx
+        Ham.W[i][1,0,:,:] = Sz
+        for k in nn[i]:
+            Ham.W[i][-1,k,:,:] = -J*Sz
+        for k in range(2,Ham.W[i].shape[0]-1):
+            Ham.W[i][k,k-1,:,:] = np.eye(2)
+        Ham.W[i][-1,-1,:,:] = np.eye(2)
+    return Ham
+
+def IsingMPO_2D_diagonal_MPS(L, h=0., J=1):
+    def diagonal_matrix(L):
+        sites = np.arange(L*L)
+        k = 1
+        pos = 0
+        dist = L-1
+        mat = np.zeros((L,L))
+        while(dist>-L):
+            mat += np.diag(sites[pos:(pos+k)],dist)
+            pos += k
+            if dist > 0:
+                k+=1
+            else:
+                k-= 1
+            dist -= 1
+        return np.fliplr(mat)
+    
+    def compute_dim_MPO(L):
+        config = diagonal_matrix(L)
+        dim = []
+        nearest = []
+        for x in range(L*L):
+            i,j = np.where(config == x)
+            i = i[0]; j = j[0]
+            nn = []
+            if i !=0: nn.append(config[i-1,j])
+            if j !=0: nn.append(config[i,j-1])
+            if i !=L-1: nn.append(config[i+1,j])
+            if j !=L-1: nn.append(config[i,j+1])
+            nn = np.array(nn)
+            nearest.append( np.array(nn[nn>config[i,j]]-config[i,j],int))
+            new_dim = int(np.max(nn-config[i,j]) + 2)
+            if x > 0:
+                if new_dim < dim[x-1][1]:
+                   dim.append((dim[x-1][1],dim[x-1][1]-1,2,2))
+                else:
+                    dim.append((dim[x-1][1],new_dim,2,2))    
+            else:
+                dim.append((1,new_dim,2,2))
+        dim[-1] = (dim[-2][1],1,2,2)
+        return dim,nearest
+    
+    Ham = MPO.MPO(L*L, 2)
+    Ham.config = np.array(diagonal_matrix(L),int)
+    # Pauli Matrices
+    Sx = np.array([[0, 1], [1, 0]])
+    Sz = np.array([[1, 0], [0,-1]])
+
+    dimW,nn = compute_dim_MPO(L)
+
+    for i,dim in enumerate(dimW):
+        Ham.W[i] = np.zeros(dim)
+    
+    # Left Bond
+    Ham.W[0][0,0,:,:] = - h*Sx
+    for k in nn[0]:
+        Ham.W[0][0,k,:,:] = -J*Sz
+    Ham.W[0][0,-1,:,:] = np.eye(2)
+    # Right Bond
+    Ham.W[-1][0,0,:,:] = np.eye(2)
+    Ham.W[-1][1,0,:,:] = Sz
+    Ham.W[-1][2,0,:,:] = -h*Sx    
+    # Bulk Bonds
+    for i in range(1,L**2-1):
+        Ham.W[i][0,0,:,:]  = np.eye(2)
+        Ham.W[i][-1,0,:,:] = -h*Sx
+        Ham.W[i][1,0,:,:] = Sz
+        for k in nn[i]:
+            Ham.W[i][-1,k,:,:] = -J*Sz
+        for k in range(2,Ham.W[i].shape[0]-1):
+            Ham.W[i][k,k-1,:,:] = np.eye(2)
+        Ham.W[i][-1,-1,:,:] = np.eye(2)
+    return Ham
---
+++ tdvp2_PartOfTheSystem.py
@@ -0,0 +1,148 @@
+import MPS_class as MPS
+
+import contraction_utilities as contract
+import numpy as np
+import numpy.linalg as LA
+from ncon import ncon
+
+from LanczosRoutines import expm_krylov_lanczos
+
+## tensor contraction for minimization
+##         +--M--+
+##         |  |  |
+## H_eff = L--H--R
+##         |  |  |
+##         +--M--+       
+def apply_Heff2(L,H,R,M):
+    return ncon([L, H, R,M], [[1,2,-1],[2,6,3,4,-2,-3],[5,6,-4],[1,3,4,5]])
+def apply_Heff1(L,H,R,M):
+    return ncon([L,H,R,M],[[1,2,-1],[2,5,3,-2],[4,5,-3],[1,3,4]])
+
+def local_exponentiation(method,M,L,H,R,delta,maxit=10):
+    if method == 'H2':
+            Afunc = lambda x: apply_Heff2(L, H, R, x.reshape(M.shape)).ravel()
+    if method == 'H1':
+            Afunc = lambda x: apply_Heff1(L, H, R, x.reshape(M.shape)).ravel()
+    v = expm_krylov_lanczos(Afunc, M.ravel(), 1j*delta/2, maxit)
+    return v/LA.norm(v)
+    
+class TDVP2:
+    def __init__(self,MPS_,H,chi_MAX=64,chi_min=0,truncate_info=True):
+        self.MPS = MPS.MPS(MPS_.L,MPS_.chim,MPS_.d)
+        self.MPS.M = MPS_.M.copy()
+        self.MPO = H
+        self.L = self.MPS.L
+        self.chi_MAX = chi_MAX
+        self.end_max = False
+        self.chi_min = chi_min
+        self.truncate_info = truncate_info
+        
+        self.W12 = [0 for i in range(self.L-1)]
+        for i in range(self.L-1):
+            self.W12[i] = ncon([H.W[i],H.W[i+1]],[[-1,1,-3,-5],[1,-2,-4,-6]])
+
+    def initialize(self):
+        L = self.L
+
+        self.RT = [0 for x in range(self.L+1)]
+        self.LT = [0 for x in range(self.L+1)]
+        
+        self.RT[L]  = np.ones((1,1,1))
+        self.LT[-1] = np.ones((1,1,1))
+        
+        # Generates R tensors
+        for j in range(L-1,1,-1):
+            self.RT[j] = contract.contract_right(self.MPS.M[j], self.MPO.W[j], self.MPS.M[j].conj(), self.RT[j+1])
+       
+    def right_sweep(self,delta,krydim,l):
+        for i in range(l-1):
+            
+            M = ncon([self.MPS.M[i],self.MPS.M[i+1]],[[-1,-2,1],[1,-3,-4]])
+            
+            shpMi = self.MPS.M[i].shape
+            shpMj = self.MPS.M[i+1].shape
+            
+            psi = local_exponentiation('H2',M, self.LT[i-1], self.W12[i], self.RT[i+2], -delta,krydim)
+            
+            M = psi.reshape(shpMi[0]*shpMi[1],shpMj[1]*shpMj[2])
+            
+            U,S,V = LA.svd(M, full_matrices=False)
+            S /= np.linalg.norm(S)
+            
+            S = S[S>1e-16]
+            chi = S.size
+            if self.truncate_info:
+                indices = np.where( (1-np.cumsum(S**2) < self.etrunc ))[0]
+                if len(indices) > 0:
+                      chi = indices[0]+1
+                else:
+                        chi = S.size
+                if chi > self.chi_MAX:
+                    chi = self.chi_MAX
+                    self.end_max = True
+                chi = np.max([chi,self.chi_min])
+            
+            chi = np.min([chi,self.chi_MAX])
+            # Truncation
+            U = U[:,:chi]
+            S = S[:chi]
+            V = V[:chi,:]
+            
+            S /= LA.norm(S)
+            A = U.reshape(shpMi[0],shpMi[1], S.size)
+            
+            self.MPS.M[i] = A
+            self.MPS.M[i+1] = (np.diag(S)@V).reshape(S.size,shpMj[1],shpMj[2])
+            
+            if i != l-2:
+                self.LT[i]  = contract.contract_left(A, self.MPO.W[i], A.conj(), self.LT[i-1])
+                shpMj = self.MPS.M[i+1].shape
+                self.MPS.M[i+1] = local_exponentiation('H1', self.MPS.M[i+1], self.LT[i], self.MPO.W[i+1], self.RT[i+2], delta,krydim).reshape(shpMj)
+            
+    def left_sweep(self,delta,krydim,l):
+        for i in range(l-1, 0, -1):
+            
+            M = ncon([self.MPS.M[i-1],self.MPS.M[i]],[[-1,-2,1],[1,-3,-4]])
+            shpMi = self.MPS.M[i-1].shape
+            shpMj = self.MPS.M[i].shape
+            
+            psi = local_exponentiation('H2',M, self.LT[i-2], self.W12[i-1], self.RT[i+1],-delta,krydim)         
+            M = psi.reshape(shpMi[0]*shpMi[1],shpMj[1]*shpMj[2])
+            
+            U,S,V = LA.svd(M,full_matrices=False)
+            S /= LA.norm(S)
+            
+            S = S[S>1e-16]
+            chi = S.size
+            if self.truncate_info:
+                indices = np.where( (1-np.cumsum(S**2) < self.etrunc ))[0]
+                if len(indices) > 0:
+                      chi = indices[0]+1
+                else:
+                        chi = S.size
+                if chi > self.chi_MAX:
+                    chi = self.chi_MAX
+                    self.end_max = True
+                chi = np.max([chi,self.chi_min])
+            
+            chi = np.min([chi,self.chi_MAX])
+            # Truncation
+            U = U[:,:chi]
+            S = S[:chi]
+            V = V[:chi,:]
+            S /= LA.norm(S)
+            B  = V.reshape(S.size, shpMj[1], shpMj[2])
+            
+            self.MPS.M[i] = B
+            self.MPS.M[i-1] = (U@np.diag(S)).reshape(shpMi[0], shpMi[1], S.size)
+            
+            if i != 1:
+                self.RT[i]  = contract.contract_right(B, self.MPO.W[i], B.conj(), self.RT[i+1])
+                shpMi = self.MPS.M[i-1].shape
+                self.MPS.M[i-1] = local_exponentiation('H1',self.MPS.M[i-1], self.LT[i-2],self.MPO.W[i-1], self.RT[i],delta,krydim).reshape(shpMi)       
+                
+    def time_step(self, delta, etrunc, krydim=10,l=-1):
+        if l == -1: l = self.L
+        self.etrunc = etrunc
+        self.right_sweep(delta,krydim,l)
+        self.left_sweep(delta,krydim,l)
